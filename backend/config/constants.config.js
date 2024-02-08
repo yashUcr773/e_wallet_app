4d1a0c8df033edb7f7e9851b3f54c79453e3d973
yashUcr773 <yagga004@ucr.edu>
2024-02-07 22:54:58 -0800

    changes made to host BE on vercel

--- backend/config/constants.config.js
+++ backend/config/constants.config.js
@@ -1,6 +1,5 @@
 const CONSTANTS = {
     MONGODBCONNECTION: process.env.mongoDBString || "123",
-    APIBASEURL: "https://localhost:3000/api/v1",
     JWTSECRET: process.env.JWTSecret || "123",
     EXPIRYTIME: 1000*60*60*24
 };
--- backend/index.js
+++ backend/index.js
@@ -8,4 +8,7 @@ const PORT = 3000;
 app.use(cors());
 app.use(bodyParser.json());
 app.use("/api/v1", rootRouter);
+app.use("/", (req, res) => {
+    res.send("Express app hosted on vercel. Code at https://github.com/yashUcr773/e_wallet_app/tree/master/backend")
+});
 app.listen(PORT, () => console.log("server started on port", PORT));
