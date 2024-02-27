fd3ac29682dce09dec57ba23f5c760118f185a3a
yashUcr773 <yagga004@ucr.edu>
2024-02-26 20:15:20 -0800

    .

--- backend/index.js
+++ backend/index.js
@@ -19,13 +19,13 @@ app.use(credentials);
 
 // Cross-Origin Resource Sharing
 app.use((req, res, next) => {
-  if (req.path === "/") {
-    // Allow all origins for the root path
-    cors()(req, res, next);
-  } else {
-    // Use CORS with options for other routes
-    cors(corsOptions)(req, res, next);
-  }
+    if (req.path.startsWith("/auth") || req.path.startsWith("/api")) {
+        // Use CORS with options for other routes
+        cors(corsOptions)(req, res, next);
+    } else {
+        // Allow all origins for the root path
+        cors()(req, res, next);
+    }
 });
 
 // built-in middleware to handle urlencoded form data
